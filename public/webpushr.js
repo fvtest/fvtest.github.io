888c75a159db14aad79ca06354e799997b9c6b93
fvtest <frankvc2016@gmail.com>
2023-12-03 18:21:30 -0400

    update site url

--- public/webpushr.js
+++ public/webpushr.js
@@ -1,9 +1,7 @@
 //start webpushr tracking code 
-(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
-fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
-webpushr('setup',{'key':'BHGt_F0uAOcXIsE7VYxH2_MwAR0zx3ouW72aUVn79ANMjsMtxGQtCjqGmR4j9I3srL-uJoL75Cffm-UHCq3NOe0' });
+(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('setup',{'key':'BLX8OgkX_RcG7WHULXxFpq2B6YURdFJKBAqqKQg75Di-2HBboRxnqqVBQ_-pDZq4FIoeywVoMeO188p2m11vf-Q' });
 // end webpushr tracking code
 
 function _webpushrScriptReady(){
-    webpushr('attributes',{"email" : "chrome_test@example.com"});
+    webpushr('attributes',{"email" : "chrome_test1@example.com"});
 }
