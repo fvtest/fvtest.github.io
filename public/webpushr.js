8b7867c1f02b32641492aaea77b6a3e3ee8b6272
fvtest <frankvc2016@gmail.com>
2023-12-03 17:56:45 -0400

    add astro test

---
+++ public/webpushr.js
@@ -0,0 +1,9 @@
+//start webpushr tracking code 
+(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
+fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
+webpushr('setup',{'key':'BHGt_F0uAOcXIsE7VYxH2_MwAR0zx3ouW72aUVn79ANMjsMtxGQtCjqGmR4j9I3srL-uJoL75Cffm-UHCq3NOe0' });
+// end webpushr tracking code
+
+function _webpushrScriptReady(){
+    webpushr('attributes',{"email" : "chrome_test@example.com"});
+}
